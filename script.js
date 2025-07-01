19d990f4feaeb4a54858fa3ee4aeef8da73a157a
benapisa <bapisa2000@gmail.com.com>
2025-07-01 11:44:43 -0400

    updates

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function showMessage(answer) {
     document.getElementById('response').textContent = `You clicked: ${answer}`;
+    const scene = document.getElementById('scene');
+    if (answer === 'Yes') {
+        // Sad scene
+        scene.innerHTML = `
+            <span class="scene-emoji">😢🌧️</span>
+            <div class="scene-text">A sad rain falls as you ponder your answer...</div>
+        `;
+    } else if (answer === 'No') {
+        // Angry scene
+        scene.innerHTML = `
+            <span class="scene-emoji">😡💥</span>
+            <div class="scene-text">An angry storm brews in the distance!</div>
+        `;
+    } else {
+        // Clear scene
+        scene.innerHTML = '';
+    }
 }
 
 function addFlower() {
