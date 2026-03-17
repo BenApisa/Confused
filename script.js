75dcd73fc1490fba61bdc2be10a45d0872228e22
benapisa <bapisa2000@gmail.com.com>
2026-03-17 18:59:09 -0400

    Updates

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function showMessage(answer) {
     document.getElementById('response').textContent = `You clicked: ${answer}`;
     const scene = document.getElementById('scene');
     if (answer === 'Yes') {
-        // Sad scene
+        // Sad scene with uploaded JPEG
         scene.innerHTML = `
-            <span class="scene-emoji">😢🌧️</span>
+            <img src="images/jarjar.jpg" alt="Jar Jar" class="scene-image" onerror="this.style.display='none'" />
             <div class="scene-text">A sad rain falls as you ponder your answer...</div>
         `;
     } else if (answer === 'No') {
@@ -12,6 +12,21 @@ function showMessage(answer) {
         scene.innerHTML = `
             <img src="images/angry.png" alt="Angry" class="scene-image" />
             <div class="scene-text">An angry storm brews in the distance!</div>
+        `;
+    } else if (answer === 'Maybe') {
+        // Play YouTube video immediately (muted to improve autoplay reliability)
+        const videoId = 'y5nROc-bN5w';
+        scene.innerHTML = `
+            <div class="video-wrap">
+                <iframe
+                    id="ytplayer"
+                    class="scene-video"
+                    src="https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1&rel=0&modestbranding=1"
+                    frameborder="0"
+                    allow="autoplay; encrypted-media; picture-in-picture"
+                    allowfullscreen>
+                </iframe>
+            </div>
         `;
     } else {
         // Clear scene
